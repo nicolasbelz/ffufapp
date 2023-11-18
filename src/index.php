c11cd664c403128c4ebec797ca9964142fc3ed63
“Nicolas <nicolasbelz10@gmail.com>
2023-11-18 11:50:26 -0500

    v1

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Vulnerable Web App</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div id="navbar">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="sql_injection.php">SQL Injection</a></li>
+            <li><a href="xss_vulnerable.php">XSS Vulnerable</a></li>
+            <li><a href="directory_listing.php">Directory Listing</a></li>
+            <li><a href="file_inclusion.php">File Inclusion</a></li>
+            <!-- Add other pages if needed -->
+        </ul>
+    </div>
+    <h1>Welcome to the Vulnerable Web App</h1>
+    <p>Select an option from the navigation bar.</p>
+</body>
+</html>
