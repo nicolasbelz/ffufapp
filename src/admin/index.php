2e76eb5fb6285beea4ec37d1c716c91ca1a117cb
“Nicolas <nicolasbelz10@gmail.com>
2023-11-19 15:19:35 -0500

    working correctly fuzzing parameters for POST&GET

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,4 +1,28 @@
-<?php include '../header.php'; ?>
+<?php 
+// Enable error reporting for debugging. Remove this line in production.
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Define the correct key
+$correct_key = '3x@mpl3_K3yP@r@m3t3r_2O23';
+
+// Initialize a variable to determine if the key is correct
+$is_authorized = false;
+
+// Check for the key in GET or POST request
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['key']) && $_POST['key'] === $correct_key) {
+    $is_authorized = true;
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['key']) && $_GET['key'] === $correct_key) {
+    $is_authorized = true;
+}
+
+// If the key is incorrect, send a 403 Forbidden status code
+if (!$is_authorized) {
+    http_response_code(403);
+    echo "Forbidden: You do not have access to read the flag.";
+    exit; // Stop script execution
+}
+?>
 
 <!DOCTYPE html>
 <html>
@@ -7,31 +31,10 @@
 </head>
 <body>
     <h1>Admin Dashboard</h1>
-    <?php
-    // Define the correct key
-    $correct_key = '3x@mpl3_K3yP@r@m3t3r_2O23';
-    
-    // Function to check the key and return true if it's correct
-    function check_key($key) {
-        global $correct_key;
-        return isset($key) && $key === $correct_key;
-    }
-
-    // Check both GET and POST for the correct key
-    $key_is_correct = $_SERVER['REQUEST_METHOD'] === 'POST' ? check_key($_POST['key']) : check_key($_GET['key']);
-
-    if ($key_is_correct) {
-        // If the correct key is provided, show the flag
-        echo "<p>Flag: {your_flag_here}</p>"; // Replace with your actual flag
-    } else {
-        // If the key is incorrect, return a 403 Forbidden status code
-        http_response_code(403);
-        echo "<p>You don't have the access to read the flag!</p>";
-        exit(); // Terminate the script to prevent further execution
-    }
-    ?>
+    <?php if ($is_authorized): ?>
+        <!-- The sensitive information is shown only when the correct key is provided -->
+        <p>Flag: {your_flag_here}</p> <!-- Replace with your actual flag -->
+    <?php endif; ?>
     <a href="settings.php">Settings</a> | <a href="users/">Manage Users</a>
 </body>
 </html>
-
-<?php include '../footer.php'; ?>
