9024f146297c4e6db57ae7c05524af69bd7a7bfb
“Nicolas <nicolasbelz10@gmail.com>
2023-11-19 12:06:17 -0500

    key paramter

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,5 +1,5 @@
-// admin/index.php
 <?php include '../header.php'; ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,9 +7,17 @@
 </head>
 <body>
     <h1>Admin Dashboard</h1>
-    <p>Welcome, admin!</p>
-    <a href="settings.php">Settings</a> |
-    <a href="users/">Manage Users</a>
+    <?php
+    // Check if the 'key' GET parameter is set and if it is the correct one
+    $correct_key = '3x@mpl3_K3yP@r@m3t3r_2O23'; // This should be a complex and secret key
+    if (isset($_GET['key']) && $_GET['key'] === $correct_key) {
+        echo "<p>Flag: {your_flag_here}</p>"; // Display the sensitive information
+    } else {
+        echo "<p>You don't have the access to read the flag!</p>"; // Access denied message
+    }
+    ?>
+    <a href="settings.php">Settings</a> | <a href="users/">Manage Users</a>
 </body>
 </html>
+
 <?php include '../footer.php'; ?>
