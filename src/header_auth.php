f7a3e2730a45fb25f30c29ebad2fe63970479a2d
“Nicolas <nicolasbelz10@gmail.com>
2023-12-03 13:51:50 -0500

    header auth

---
+++ src/header_auth.php
@@ -0,0 +1,11 @@
+<?php
+// header_auth.php
+$expectedToken = '4b82Km29Fv6zQ3xT8pW5Jr7Hn'; // The expected token value
+
+if (isset($_SERVER['HTTP_X_CUSTOM_AUTH']) && $_SERVER['HTTP_X_CUSTOM_AUTH'] === $expectedToken) {
+    echo "Authentication successful. Access granted to sensitive content.";
+    // Include sensitive content or operations here
+} else {
+    http_response_code(401); // Unauthorized
+    echo "Authentication failed.";
+}
